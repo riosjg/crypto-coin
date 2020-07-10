e1cfa667fedab1f1531cf00544abcc7f688aa1b2
Gextox <gabriel_il@live.com>
2020-07-10 01:47:47 -0300

    Updates

---
+++ precache-manifest.03b61b5d67acbab7b069219d2444b8ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2b02424869b569e4e5a1ee25b515e3",
+    "url": "/crypto-coin/index.html"
+  },
+  {
+    "revision": "f14b556ce55d655e28a9",
+    "url": "/crypto-coin/static/css/main.90d87640.chunk.css"
+  },
+  {
+    "revision": "27a25743d8ff4cf45990",
+    "url": "/crypto-coin/static/js/2.7b67d9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto-coin/static/js/2.7b67d9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14b556ce55d655e28a9",
+    "url": "/crypto-coin/static/js/main.3cd779b5.chunk.js"
+  },
+  {
+    "revision": "d927ab07b9dec03160f9",
+    "url": "/crypto-coin/static/js/runtime-main.9c2f11d3.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/crypto-coin/static/media/crypto.96e45d69.png"
+  }
+]);
